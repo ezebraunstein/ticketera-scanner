d31fbe76922f5a9e6f593a221610f6ee275c9590
Ezequiel Braunstein <ezequiel.braunstein@gmail.com>
2023-11-14 16:59:53 -0300

    ...

--- app.config.js
+++ app.config.js
@@ -33,6 +33,9 @@ export default {
         extra: {
             REACT_APP_ACCESS_KEY: process.env.REACT_APP_ACCESS_KEY,
             REACT_APP_SECRET_ACCESS_KEY: process.env.REACT_APP_SECRET_ACCESS_KEY,
+            eas: {
+                projectId: "667abad9-b81c-49d9-9d65-1aa50284d4a5"
+            }
         }
     }
 }
